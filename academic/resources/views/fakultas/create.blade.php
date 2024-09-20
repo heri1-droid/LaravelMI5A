42fb37936630239bde534f4727980e785dc8d1d7
heri <heri.cyber186@gmail.com>
2024-09-20 14:28:38 +0700

    pindah2
    ok

--- academic/resources/views/fakultas/create.blade.php
+++ academic/resources/views/fakultas/create.blade.php
@@ -5,6 +5,16 @@
 <h4>Fakultas</h4>
 
 
-<form action="{{route('fakultas.store')}}" method="POST"> </form>
+<form action="{{route('fakultas.store')}}" method="POST"> 
+    @csrf
+    Nama
+    <input type="text" name="nama" id="" class="form-control">
+    Dekan
+    <input type="text" name="nama" id="" class="form-control">
+    Singkatan
+    <input type="text" name="nama" id="" class="form-control">
+
+    <button type="submit" class="btn btn-primary">Simpan</button>
+</form>
 
 @endsection
