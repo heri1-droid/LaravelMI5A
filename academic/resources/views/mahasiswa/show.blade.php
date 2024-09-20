cc0e7376e32f75dc79402603302af43ef3bcfa07
heri <heri.cyber186@gmail.com>
2024-09-20 14:07:59 +0700

    pindah
    oke

--- academic/resources/views/mahasiswa/show.blade.php
+++ academic/resources/views/mahasiswa/show.blade.php
@@ -2,5 +2,44 @@
 
 @section('content')
     <h4>{{$mahasiswa['nama']}}</h4>
+        <!-- <h4>{{$mahasiswa['nama']}}</h4>
+    <h4>{{$mahasiswa['nama']}}</h4>
+    <h4>{{$mahasiswa['nama']}}</h4>
+    <h4>{{$mahasiswa['nama']}}</h4> -->
+
+    <table class="table table-striped">
+        <tr>
+            <td>Program Studi</td>
+            <td>{{$mahasiswa['prodi']['nama']}}</td>
+        </tr>
+        <tr>
+            <td>NPM</td>
+            <td>{{$mahasiswa['npm']}}</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>{{$mahasiswa['nama']}}</td>
+        </tr>
+        <tr>
+            <td>Tempat Lahir</td>
+            <td>{{$mahasiswa['tempat_lahir']}}</td>
+        </tr>
+        <tr>
+            <td>Tanggal Lahir</td>
+            <td>{{$mahasiswa['tanggal_lahir']}}</td>
+        </tr>
+        <tr>
+            <td>email</td>
+            <td>{{$mahasiswa['email']}}</td>
+        </tr>
+        <tr>
+            <td>No Hp</td>
+            <td>{{$mahasiswa['hp']}}</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>{{$mahasiswa['alamat']}}</td>
+        </tr>
+    </table>
 
 @endsection
