d0cbef84c9e346f1e97181c5df73351a08594a9f
heri1-droid <heri.cyber186@gmail.com>
2024-11-15 15:23:41 +0700

    aa

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,13 @@ Route::get('fakultas', [FakultasController::class, 'getFakultas'])->middleware([
 Route::get('prodi', [ProdiController::class, 'getProdi'])->middleware('auth:sanctum');
 Route::get('mahasiswa', [MahasiswaController::class, 'getMahasiswa'])->middleware('auth:sanctum');
 
-Route::post('fakultas', [FakultasController::class, 'storeFakultas'])->middleware(['auth:sanctum', 'ability:create']);;
+Route::post('fakultas', [FakultasController::class, 'storeFakultas'])->middleware(['auth:sanctum', 'ability:create']);
 Route::post('prodi', [ProdiController::class, 'storeProdi']);
 
 Route::delete('fakultas', [FakultasController::class, 'storeFakultas']);
 
 Route::post('login', action: [AuthController::class, 'login']);//
-Route::post('mahasiswa', action: [MahasiswaController::class, 'storeMahasiswa']);//
+Route::post('mahasiswa', action: [MahasiswaController::class, 'storeMahasiswa'])//
 
 //Route::get('register', [AuthController::class, 'register']);gfh
 
